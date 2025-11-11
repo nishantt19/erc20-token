7648016796a9e3be780c905bc18c49e133814885
Nishant Yadav <nishantyadav@Nishants-MacBook-Air.local>
2025-11-11 16:32:09 +0530

    chore: initial project setup

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center flex-col gap-y-2 h-screen">
+      <h1>Welcome to ERCTransfer</h1>
+      <p>A dApp to transfer ERC20 tokens with ease.</p>
+    </main>
+  );
+}
